b4908969927791f58ae29e8e96477e120f18430c
Luke Bhangyi <bhangyiluke@gmail.com>
2024-08-01 15:53:29 +0400

    What has been done, so far

--- src/main/java/_07_05/FilesExercise.java
+++ src/main/java/_07_05/FilesExercise.java
@@ -10,14 +10,18 @@ public class FilesExercise {
     public static void main(String[] args) {
         // Create a new empty file called example.txt inside folderA
         try {
-           Path dirA= Files.createDirectories(Path.of("src/main/java/_07_05/folderA"));
-           Path dirB= Files.createDirectories(Path.of("src/main/java/_07_05/folderB"));
-            if(Files.notExists(Paths.get("src/main/java/_07_05/folderA/example.txt"))){
-                Files.createFile(Paths.get("src/main/java/_07_05/folderA/example.txt"));
+            Path dirA = Files.createDirectories(Paths.get("src/main/java/_07_05/folderA"));
+            Path dirB = Files.createDirectories(Paths.get("src/main/java/_07_05/folderB"));
+            Path fileA = Paths.get(dirA + "/example.txt");
+            Path fileB = Paths.get(dirB + "/example.txt");
+            if (Files.notExists(fileA)) {
+                fileA = Files.createFile(fileA);
             }
-                
+
             // Copy the file to folderB
-            Files.copy(file, Paths.get("src/main/java/_07_05/folderB/example.txt"));
+            if (Files.notExists(fileB)) {
+                Files.copy(fileA, fileB);
+            }
             // List the contents of folderB to check that your file is in there
             Files.list(dirB).forEach(System.out::print);
         } catch (IOException e) {
